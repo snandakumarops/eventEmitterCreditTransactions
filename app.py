1127b7dd3b5d1ecde66628c037c2e021083346ed
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2019-02-01 11:30:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def main(args):
 
     logging.info('begin sending events')
     while True:
-        producer.send(args.topic, json.dumps(generate_event()).encode(), "EVENT"+str(uuid.uuid4()))
+        producer.send(args.topic, json.dumps(generate_event()).encode())
         logging.info(generate_event())
         time.sleep(1.0 / int(args.rate))
     logging.info('end sending events')
