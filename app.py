28179f00996292ad82e47f1b210ca82c6e5ed8b7
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2019-02-04 11:38:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def main(args):
     while True:
         producer.send(args.topic, json.dumps(generate_event()).encode())
         logging.info(generate_event())
-        time.sleep(30.0)
+        time.sleep(120.0)
     logging.info('end sending events')
 
 
